d850782094bde8227c68439deea6b908655a7930
Justice Velasco <justicevgomez001@gmail.com>
2021-10-16 19:50:53 +0200

    dinamic like button

--- components/Post/index.js
+++ components/Post/index.js
@@ -1,61 +1,84 @@
 import React, { useState } from "react";
-import { View, Text, TouchableWithoutFeedback, Image } from "react-native";
-import { Video } from "expo-av"; 
+import { View, Text, TouchableWithoutFeedback, Image, TouchableOpacity } from "react-native";
+import { Video } from "expo-av";
 import { Ionicons, AntDesign, FontAwesome, Fontisto } from "@expo/vector-icons";
 import styles from "./styles";
 
-const Post = () => {
+const Post = (props) => {
+  
+  const [post, setPost] = useState(props.post);
+  const [isLiked, setIsLiked] = useState(false);
   const [status, setStatus] = useState({});
   const video = React.useRef(null);
 
+  const onLikePress = () => {
+    const likesToAdd = isLiked ? -1 : 1;
+    setPost({
+      ...post,
+      likes: post.likes + likesToAdd,
+    });
+    setIsLiked(!isLiked);
+  }
+
   return (
     <View style={styles.container}>
-        <TouchableWithoutFeedback onPress={() => status.isPlaying 
-          ? video.current.pauseAsync() : video.current.playAsync()}>
-          <Video
-            style={styles.video}
-            source={{
-              uri: "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/ForBiggerMeltdowns.mp4",
-            }}
-            shouldPlay
-            resizeMode="cover"
-            isLooping
-            ref={video}
-            onPlaybackStatusUpdate={(status) => setStatus(() => status)}
+      <TouchableWithoutFeedback
+        onPress={() =>
+          status.isPlaying
+            ? video.current.pauseAsync()
+            : video.current.playAsync()
+        }
+      >
+        <Video
+          style={styles.video}
+          source={{uri: post.videoUri}}
+          shouldPlay
+          resizeMode="cover"
+          isLooping
+          ref={video}
+          onPlaybackStatusUpdate={(status) => setStatus(() => status)}
+        />
+      </TouchableWithoutFeedback>
+
+      <View style={styles.uiContainer}>
+        <View style={styles.feedbackContainer}>
+          <Image
+            style={styles.profilePicture}
+            source={{uri: post.user.imageUri}}
           />
-        </TouchableWithoutFeedback>
+          <TouchableOpacity onPress={onLikePress} style={{alignItems: "center"}}>
+            <AntDesign name="heart" size={35} color={isLiked ? "red" : "white"} />
+            <Text style={{ color: "white" }}>{post.likes}</Text>
+          </TouchableOpacity>
 
-        <View style={styles.uiContainer}>
-          <View style={styles.feedbackContainer}>
-            <Image style={styles.profilePicture} source={{uri:"https://aot-wpengine.netdna-ssl.com/wp-content/uploads/2014/04/animal_facts-e1396431549968.jpg"}} />
-            <AntDesign name="heart" size={35} color="white" />
-            <Text style={{color: "white"}}>21.4k</Text>
-            <View style={{marginVertical: 25, alignItems: "center"}}>
-              <FontAwesome name="commenting" size={37} color="white" />
-              <Text style={{color: "white"}}>864</Text>
-            </View>
-            <Fontisto name="share-a" size={30} color="white" />
-            <Text style={{color: "white"}}>53k</Text>
+          <View style={{ marginVertical: 25, alignItems: "center" }}>
+            <FontAwesome name="commenting" size={37} color="white" />
+            <Text style={{ color: "white" }}>{post.comments}</Text>
           </View>
 
-          <View style={styles.bottomInfo}>
-            <View style={styles.infoContainer}>
-              <Text style={styles.username}>@pepo_28</Text>
-              <Text style={styles.description}>
-                lorem ipsumawsfg asgsdgs dfgh drh ipsu mawsf gasg sdg sdfgh drh
-                ipsu ma wsfgas gsdgs dfgh drh
-              </Text>
-              <View style={styles.musicContainer}>
-                <Ionicons name="musical-notes" style={{ margin: 8 }} size={18} color="white"/>
-                <Text style={{ color: "white", fontSize: 11 }}>Post Malone - Motley Crew</Text>
-              </View>
-            </View>
-            <Image style={styles.musicDisc} source={{uri:"https://aot-wpengine.netdna-ssl.com/wp-content/uploads/2014/04/animal_facts-e1396431549968.jpg"}} />
-          </View>
+          <Fontisto name="share-a" size={30} color="white" />
+          <Text style={{ color: "white" }}>{post.shares}</Text>
         </View>
+
+        <View style={styles.bottomInfo}>
+          <View style={styles.infoContainer}>
+            <Text style={styles.username}>@{post.user.username}</Text>
+            <Text style={styles.description}>{post.description}</Text>
+            <View style={styles.musicContainer}>
+              <Ionicons
+                name="musical-notes"
+                style={{ margin: 8 }}
+                size={18}
+                color="white"
+              />
+              <Text style={{ color: "white", fontSize: 11 }}>{post.songName}</Text>
+            </View>
+          </View>
+          <Image style={styles.musicDisc} source={{uri: post.songImage}}/>
+        </View>
+      </View>
     </View>
   );
-  
 };
 
 export default Post;
--- screens/Home/index.js
+++ screens/Home/index.js
@@ -1,13 +1,32 @@
-import React from 'react'
-import { View } from 'react-native'
-import Post from '../../components/Post';
+import React from "react";
+import { View } from "react-native";
+import Post from "../../components/Post";
+
+const post1 = {
+  id: "p1",
+  videoUri:
+    "http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/VolkswagenGTIReview.mp4",
+  user: {
+    id: "u1",
+    username: "daviddobrik",
+    imageUri:
+      "https://i.scdn.co/image/ab67706c0000bebbd7ec3f012f1fd1e4437d5bc8",
+  },
+  description: "hahahah oh boy @borat",
+  songName: "NF - The search",
+  songImage:
+    "https://i.scdn.co/image/ab67706c0000bebbd7ec3f012f1fd1e4437d5bc8",
+  likes: 123,
+  comments: 12,
+  shares: 44,
+};
 
 const Home = () => {
-    return (
-        <View>
-            <Post />
-        </View>
-    )
-}
+  return (
+    <View>
+      <Post post={post1} />
+    </View>
+  );
+};
 
-export default Home
+export default Home;
